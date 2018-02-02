32e944d26aa3d1e938d65fada76b35a4db661175
Justin.wang <wangrongchao@126.com>
2018-02-02 15:23:09 +0800

    first commit

---
+++ JWAppIconUpdate/Classes/JWAppIconUpdate.h
@@ -0,0 +1,12 @@
+//
+//  JWAppIconUpdate.h
+//  JWAppIconUpdate
+//
+//  Created by Justin.wang on 2018/2/2.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JWAppIconUpdate : NSObject
+
+@end
---
+++ JWAppIconUpdate/Classes/JWAppIconUpdate.m
@@ -0,0 +1,12 @@
+//
+//  JWAppIconUpdate.m
+//  JWAppIconUpdate
+//
+//  Created by Justin.wang on 2018/2/2.
+//
+
+#import "JWAppIconUpdate.h"
+
+@implementation JWAppIconUpdate
+
+@end
